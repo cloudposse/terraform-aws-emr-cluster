f5f6eac6e83fb03c847fe48738ddd15f8c1b381e
aknysh <andriy.knysh@gmail.com>
2022-08-11 22:04:04 -0400

    Update all modules, examples and tests to the latest versions and patterns

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -45,6 +45,8 @@ module "aws_key_pair" {
   attributes          = ["ssh", "key"]
   ssh_public_key_path = var.ssh_public_key_path
   generate_ssh_key    = var.generate_ssh_key
+
+  context = module.this.context
 }
 
 module "emr_cluster" {
